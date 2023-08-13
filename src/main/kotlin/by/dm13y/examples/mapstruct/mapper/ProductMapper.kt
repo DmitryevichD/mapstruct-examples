5a235151bc5b9ceb806746de8e2ca4461f696eae
DmitrievichD <d.dmitrievich@itransition.com>
2023-08-13 13:55:46 +0300

    add mapping field when field names is different

--- src/main/kotlin/by/dm13y/examples/mapstruct/mapper/ProductMapper.kt
+++ src/main/kotlin/by/dm13y/examples/mapstruct/mapper/ProductMapper.kt
@@ -4,8 +4,11 @@ import by.dm13y.examples.mapstruct.config.mapper.ProductMapperConfig
 import by.dm13y.examples.mapstruct.model.dto.ProductDto
 import by.dm13y.examples.mapstruct.model.entity.Product
 import org.mapstruct.Mapper
+import org.mapstruct.Mapping
 
 @Mapper(componentModel = "spring", config = ProductMapperConfig::class)
 interface ProductMapper {
+
+    @Mapping(source = "productName", target = "name")
     fun toProduct(productDto: ProductDto): Product
 }
